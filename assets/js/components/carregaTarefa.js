83da9fc8e3a8334ede4c075fe740519e57ac0cbe
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2023-10-21 14:38:06 -0300

    Ordenação das tarefas por data

--- assets/js/components/carregaTarefa.js
+++ assets/js/components/carregaTarefa.js
@@ -1,5 +1,5 @@
 import { criaData } from './criaData.js'
-import { removeDatasRepetidas } from '../service/data.js'
+import { ordenaDatas, removeDatasRepetidas } from '../service/data.js'
 
 export const carregaTarefa = () =>{
     const lista = document.querySelector('[data-list]')
@@ -9,6 +9,8 @@ export const carregaTarefa = () =>{
     lista.innerHTML = " "
  
     const datasUnicas = removeDatasRepetidas(tarefasCadastradas)
+
+    ordenaDatas(datasUnicas)
 
     datasUnicas.forEach((dia) => {
       lista.appendChild(criaData(dia))
