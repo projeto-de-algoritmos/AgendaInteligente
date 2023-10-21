43234239286835e61b73f28d62f547cf03b0e105
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2023-10-21 00:45:30 -0300

    Reestruturação do código

---
+++ assets/js/components/criaTarefa.js
@@ -0,0 +1,51 @@
+import BotaoConclui from './concluiTarefa.js'
+import BotaoDeleta from './deletaTarefa.js'
+
+export const handleNovoItem = (evento) => {
+    evento.preventDefault()
+
+    const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value)
+    const dataFormatada = data.format('DD/MM/YYYY')
+
+    const initDate = document.querySelector('[data-form-initDate]').value
+    const endDate = document.querySelector('[data-form-endDate]').value
+
+    const dados = {
+        valor,
+        dataFormatada,
+        initDate,
+        endDate
+    }
+
+    const tarefasAtualizadas = [...tarefas, dados]
+
+    const criaTarefa = Tarefa(dados);
+    
+    lista.appendChild(criaTarefa)
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefasAtualizadas))
+
+    input.value = " "
+
+}
+
+export const Tarefa = ({ valor, dataFormatada, initDate, endDate}) => {
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${dataFormatada} <br> inicio: ${initDate}h fim: ${endDate}h <br> ${valor}</p>`
+
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
+
+    return tarefa;
+}
