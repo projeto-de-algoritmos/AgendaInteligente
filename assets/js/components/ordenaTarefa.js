ae9e2b1d188a93bf726b5eb09498013585143019
Lucas Spinosa <lucasspinosa.software@gmail.com>
2023-10-23 00:08:13 -0300

    Tarefas concluídas não entram na otimização

--- assets/js/components/ordenaTarefa.js
+++ assets/js/components/ordenaTarefa.js
@@ -21,24 +21,29 @@ const ordenarTarefa = (evento) => {
    const diaAgenda = listaTarefas.querySelector((".content-data")).textContent;
    
    const tarefas = listaTarefas.querySelectorAll(("li"));
+
    const tarefaObjetos = new Array;
    const tarefasPossiveis = new Array;
 
    for(let i = 0; i < tarefas.length; i++){
-      let descricaoTarefa = tarefas[i].querySelector((".content")).textContent;
-      let inicioTarefa = tarefas[i].querySelector((".horarioInicio")).textContent;
-      let fimTarefa = tarefas[i].querySelector((".horarioFim")).textContent;
 
-      inicioTarefa = inicioTarefa.slice(8).replaceAll(':', '');
-      fimTarefa = fimTarefa.slice(5).replaceAll(':', '');
-
-      const objetoTarefa = {
-         descricao: descricaoTarefa,
-         horarioInicio: inicioTarefa,
-         horarioFinal: fimTarefa
+      if(!tarefas[i].classList.contains("done")){
+         let descricaoTarefa = tarefas[i].querySelector((".content")).textContent;
+         let inicioTarefa = tarefas[i].querySelector((".horarioInicio")).textContent;
+         let fimTarefa = tarefas[i].querySelector((".horarioFim")).textContent;
+   
+         inicioTarefa = inicioTarefa.slice(8).replaceAll(':', '');
+         fimTarefa = fimTarefa.slice(5).replaceAll(':', '');
+   
+         const objetoTarefa = {
+            descricao: descricaoTarefa,
+            horarioInicio: inicioTarefa,
+            horarioFinal: fimTarefa
+         }
+   
+         tarefaObjetos.push(objetoTarefa);
       }
 
-      tarefaObjetos.push(objetoTarefa);
       
    }
 
